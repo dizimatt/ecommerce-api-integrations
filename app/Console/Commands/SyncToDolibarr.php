1a81990badef60d2c741c02288386d5d184192dd
Matt Dilley <dizimatt74@gmail.com>
2022-03-19 11:05:25 +1100

    updating gitignore

---
+++ app/Console/Commands/SyncToDolibarr.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\AbstractCommand;
+
+class SyncToDolibarr extends AbstractCommand
+{
+    const PROGRESS_BAR_FORMAT = 'debug';
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dolibarr:to:syncproducts
+                                {store_id : The integrations Store ID for the Shopify Store}
+                                {product_id? : The integrations Store ID for the Shopify Store}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Function Test command';
+
+    /**
+     * Execute the console command.
+     *
+     * @param  \App\DripEmailer $drip
+     * @return mixed
+     */
+    public function handle()
+    {
+        $time_start = microtime(true);
+
+        parent::handle();
+
+        echo "\n\n";
+        $this->info('SyncToDolibarr::handle() EXECUTED');
+        echo "\n";
+
+        // ----------------------------------------------------------------------
+        // Test code here
+        // ----------------------------------------------------------------------
+
+        \App\Services\Dolibarr\to\SyncProducts::execute('6632857862328');
+//        \App\Services\Dolibarr\to\SyncProducts::execute();
+        return;
+    }
+}
