49e4823094d66c11dd3cf436edc1b2c703830d9f
Matt Dilley <dizimatt74@gmail.com>
2022-03-26 13:45:07 +1100

    seperated out shopify helpers from the store helpers

--- app/helpers.php
+++ app/helpers.php
@@ -143,28 +143,6 @@ if (!function_exists('store')) {
     }
 }
 
-if (!function_exists('shopify')) {
-    function shopify()
-    {
-        // Fetch from Singleton
-        $shopify = config('shopify', false);
-
-        if (!$shopify) {
-            // Initialise Shopify API Client Singleton
-            $shopify = new \App\Shopify\Client();
-            
-            $shopify->setShop(store()->hostname);
-            $shopify->setAccessToken(store()->access_token);
-            $shopify->setVersion(env('SHOPIFY_APP_API_VERSION'));
-            $shopify->startSession();
-
-            // Store as a Singleton
-            config(['shopify' => $shopify]);
-        }
-
-        return $shopify;
-    }
-}
 
 if (!function_exists('setShopifyTopics')) {
     function setShopifyTopics(array $shopifyTopics)
